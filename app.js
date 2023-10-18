5647f7a8395a7d9b28daded1acbf53595687a7ec
Jules <dayumwallace@hotmail.co.uk>
2023-10-18 16:45:11 +0100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Howdy Universe! May U continue working in MY favour!'));
+app.get('/', (req, res) => res.send('Howdy Universe! This is my first express.js pipeline on AWS!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
