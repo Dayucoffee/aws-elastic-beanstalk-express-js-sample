af25e78006eec53e2c6c563c00b2cedf3327a24c
Jules! <69649546+Dayucoffee@users.noreply.github.com>
2023-10-19 17:28:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Howdy Universe! This is my first express.js pipeline on AWS!'));
+app.get('/', (req, res) => res.send('Howdy Universe! Project done; testing now!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
